7b80093e175ad100b520802c75b11c95cc49b1b7
Mike Isman <mikeisman@gmail.com>
2016-05-03 15:30:12 -0400

    Don't error if the channel is empty

--- leaderboard/db/queries.js
+++ leaderboard/db/queries.js
@@ -36,7 +36,7 @@ exports.officeTotal = function(table_name) {
           sum(case when exercise = 'sit ups' and completed_at is not null then reps else 0 end) as situps,
           sum(case when exercise = 'lunges' and completed_at is not null then reps else 0 end) as lunges,
           sum(case when exercise = 'burpees' and completed_at is not null then reps else 0 end) as burpees,
-          count(case when completed_at is not null then 1 else null end) * 100 / count(*) as completion_pct,
+          count(case when completed_at is not null then 1 else null end) * 100 / (case when count(*) > 0 then count(*) else 1 end) as completion_pct,
           count(*) total_assigned
         from ${table_name}
     ) x
